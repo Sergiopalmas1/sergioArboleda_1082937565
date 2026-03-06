08e8be7b50663c42418c4f76772495de85772d0e
sergio palma <sergio.palma01@usa.edu.co>
2026-03-06 08:02:28 -0500

    fixed

---
+++ sumedenumeros.py
@@ -0,0 +1,5 @@
+# Programa que pide números al usuario hasta que escriba 0
+# y muestra la suma total de los ingresados.
+
+pares = 0
+impares = 0     for
---
+++ tablas de multiplicar.py
@@ -0,0 +1,7 @@
+
+# Tablas de multiplicar del 1 al 10 usando bucles anidados
+print("\nTablas de multiplicar del 1 al 10:")
+for i in range(1, 11):
+    print(f"\nTabla del {i}:")
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i*j}")
